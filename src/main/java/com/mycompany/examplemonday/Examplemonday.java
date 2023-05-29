b0d9037ab039be3c4b0a25e79a429801236f33bb
Paarth001 <iamperry002@sheridancollege.ca>
2023-05-29 17:08:12 -0400

    student details version 1

---
+++ src/main/java/com/mycompany/examplemonday/Examplemonday.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examplemonday;
+
+/**
+ *
+ * @author 14377
+ */
+public class Examplemonday {
+
+    public static void main(String[] args) {
+        System.out.println("my name is paarth!");
+    }
+}
